6354ffefcc2b97989c27f40bb3f50ab94c2b6d32
Gonzalo Bonora <gebonora@gmail.com>
2021-10-07 15:17:59 -0300

    xd

--- typing-game/src/app/app.component.ts
+++ typing-game/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onChangeText(value: string) {
+    this.enteredText = value;
+  }
+  compare(letterFromRandom: string, letterEntered: string) {
+    if (!letterEntered) {
+      return 'pending';
+    }
+    return letterFromRandom === letterEntered ? 'correct' : 'incorrect';
+  }
 }
