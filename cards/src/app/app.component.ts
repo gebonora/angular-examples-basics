c8806ca76a84181d50c386bb6a13f5ef89094247
Gonzalo Bonora <gebonora@gmail.com>
2021-10-07 12:44:27 -0300

    Show data in card using input decorator

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageURL: 'assets/tree.jpeg',
+      username: '@nature',
+      content: 'I saw this neat three today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageURL: 'assets/mountain.jpeg',
+      username: '@mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageURL: 'assets/biking.jpeg',
+      username: '@biking1222',
+      content: 'I did some biking today',
+    },
+  ];
 }
