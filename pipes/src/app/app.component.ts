0a8d5f737389a5f358ae392b50e7ec49706697d7
Gonzalo Bonora <gebonora@gmail.com>
2021-10-07 16:32:29 -0300

    Add datepicker

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name: string = '';
+  date: string = '';
 
   onNameChange(value: string) {
     this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
   }
 }
